1bff540885874a4977fb6c3f86a4beba5fb431a6
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-10-10 11:06:18 +0200

    Update Main.py

--- Ejercicio 1 - Publicaciones/Main.py
+++ Ejercicio 1 - Publicaciones/Main.py
@@ -6,11 +6,17 @@ print("Welcome!")
 while option != 0:
     list()
     option = int(print("Choose an option: "))
-    if option == 1
-    if option == 2
-    if option == 3
-    if option == 4
-    if option == 5
-    if option == 6
+    if option == 1:
+        show()
+    if option == 2:
+        show()
+    if option == 3:
+        add()
+    if option == 4:
+        mofidy()
+    if option == 5:
+        modify()
+    if option == 6:
+        delete()
     if option == 0:
         print("Have a nice day!")
