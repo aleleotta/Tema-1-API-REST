6bfa3d5e07f4c3f016afb2427c1818fe90ddb319
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-10-10 11:03:39 +0200

    .

---
+++ Ejercicio 1 - Publicaciones/Main.py
@@ -0,0 +1,16 @@
+from Functions import *
+
+dictionary = {}
+option = 10
+print("Welcome!")
+while option != 0:
+    list()
+    option = int(print("Choose an option: "))
+    if option == 1
+    if option == 2
+    if option == 3
+    if option == 4
+    if option == 5
+    if option == 6
+    if option == 0:
+        print("Have a nice day!")
