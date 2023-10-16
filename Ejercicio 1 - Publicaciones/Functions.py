1c602bd8848ad39e6db4d12ca96335ea4e910e8b
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-10-16 11:08:31 +0200

    .

--- Ejercicio 1 - Publicaciones/Functions.py
+++ Ejercicio 1 - Publicaciones/Functions.py
@@ -2,7 +2,7 @@ import requests
 
 #url = "https://jsonplaceholder.typicode.com/"
 
-def list():
+def list(): #List of functions
     print("List:")
     print("\t1) Show all posts")
     print("\t2) Show a specific post")
@@ -13,21 +13,21 @@ def list():
     print("\t0) Exit program")
 
 """Functions"""
-def showAll():
+def showAll(): #Shows all posts.
     GET()
     
-def show(id):
+def show(id): #Asks for ID to get a single post from the JSON file.
     id = int(input("Introduce the ID: "))
     GET1(id)
     
-def add():
+def add(): #Adds a new post into the JSON file.
     userId = int(input("Introduce a user ID: "))
     id = int(input("Introduce an ID: "))
     title = input("Introduce a title: ")
     body = input("Introduce a whole text: ")
     POST(userId, id, title, body)
     
-def modifyAll():
+def modifyAll(): #Modifies all attributes of a single post through PUT.
     print("Introduce the data of the post below to modify.")
     userId = int(input("User ID: "))
     id = int(input("ID: "))
@@ -35,9 +35,9 @@ def modifyAll():
     body = input("Body: ")
     PUT(userId, id, title, body)
     
-def modify():
+def modify(): #Modifies a single attribute in a single post.
     id = int(input("Introduce the ID of the instance to modify: "))
-    attributeType = int(input("What attribute do you want to modify? 1) User ID 2) ID 3) Title 4) Body"))
+    attributeType = int(input("What attribute do you want to modify? 1) User ID 2) ID 3) Title 4) Body")) #Parameter for PATCH function to indicate attribute to modify.
     if attributeType == 1:
         attribute = int(input("UserID: "))
     if attributeType == 2:
@@ -48,7 +48,7 @@ def modify():
         attribute = input("Body: ")
     PATCH(id, attributeType, attribute)
     
-def delete():
+def delete(): #Removes a post from the JSON file by asking for post ID.
     id = int(input("Introduce the ID of the post that you would like to delete: "))
     DELETE(id)
     
@@ -57,45 +57,47 @@ def GET():
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
     print("Status code: ", response.status_code)
-    dictIsFilled = False
-    if response.status_code == 200:
+    if response.status_code == 200: #Checking for correct code, if not the same code than it displays an error message.
         print(response.json())
     else:
         print("The URL is not valid.") 
 
 def GET1(id):
     url = "https://jsonplaceholder.typicode.com/posts"
-    chosenId = "/" + str(id)
+    chosenId = "/" + str(id) #Updates current path to indicate which post the id is referring to.
     url = url + chosenId
     response = requests.get(url)
     print("Status code: ", response.status_code)
-    print(response.json())
+    if response.status_code == 200: #Checking for correct code, if not the same code than it displays an error message.
+        print(response.json())
+    else:
+        print("The URL is not valid.") 
     
 def POST(userId1, id1, title1, body1):
     url = "https://jsonplaceholder.typicode.com/posts"
     post = {'userId': userId1, 'id': id1, 'title': title1, 'body': body1}
     response = requests.post(url, json=post)
     print("Status code: ", response.status_code)
-    if response.status_code == 201:
+    if response.status_code == 201: #Checking for correct code, if not the same code than it displays an error message.
         print(response.json())
     else:
         print("The post couldn't get uploaded.")
     
 def PUT(userId1, id1, title1, body1):
     url = "https://jsonplaceholder.typicode.com/posts"
-    chosenId = "/" + str(id1)
+    chosenId = "/" + str(id1) #Updates current path to indicate which post the id is referring to.
     url = url + chosenId
     update = {'userId': userId1, 'id': id1, 'title': title1, 'body': body1}
     response = requests.put(url, json=update)
     print("Status code: ", response.status_code)
-    if response.status_code == 200:
+    if response.status_code == 200: #Checking for correct code, if not the same code than it displays an error message.
         print(response.json())
     else:
         print("The post couldn't get updated.")
     
 def PATCH(id1, attributeType, attribute):
     url = "https://jsonplaceholder.typicode.com/posts"
-    chosenId = "/" + str(id1)
+    chosenId = "/" + str(id1) #Updates current path to indicate which post the id is referring to.
     url = url + chosenId
     if attributeType == 1:
         update = {'userId': attribute}
@@ -107,18 +109,18 @@ def PATCH(id1, attributeType, attribute):
         update = {'body': attribute}
     response = requests.patch(url, json=update)
     print("Status code: ", response.status_code)
-    if response.status_code == 200:
+    if response.status_code == 200: #Checking for correct code, if not the same code than it displays an error message.
         print(response.json())
     else:
         print("The post couldn't get updated.")
     
 def DELETE(id):
     url = "https://jsonplaceholder.typicode.com/posts"
-    chosenId = "/" + str(id)
+    chosenId = "/" + str(id) #Updates current path to indicate which post the id is referring to.
     url = url + chosenId
     response = requests.delete(url)
     print("Status code: ", response.status_code)
-    if response.status_code == 200:
+    if response.status_code == 200: #Checking for correct code, if not the same code than it displays an error message.
         print(response.json())
     else:
         print("The post couldn't get deleted.")
