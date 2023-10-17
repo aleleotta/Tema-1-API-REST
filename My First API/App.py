828640504c40ce2d5f38b32b48235f886ad11bae
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-10-17 10:50:18 +0200

    .

--- My First API/App.py
+++ My First API/App.py
@@ -2,12 +2,6 @@ from flask import *
 
 app = Flask(__name__)
 
-countries = [
-    {"id": 1, "name": "Thailand", "capital": "Bangkok", "area": 513120},
-    {"id": 2, "name": "Australia", "capital": "Canberra", "area": 7617930},
-    {"id": 3, "name": "Egipto", "capital": "Cairo", "area": 1010408},
-]
-
 @app.route("/")
 def index():
     return "<h1><i>Hello everyone! :D</i></h1><br><br><br><h3><b>Feel free to do whatever you want on this page.</b></h3>"
