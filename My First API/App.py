281934b8a568080378f6de9652d7c2216ea6cd60
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-10-17 11:14:34 +0200

    Update App.py

--- My First API/App.py
+++ My First API/App.py
@@ -2,30 +2,46 @@ from flask import *
 
 app = Flask(__name__)
 
+fileName = "My First API\\Countries.json"
+
+def readFile():
+    file = open(fileName, "r")
+    countries = json.load(file)
+    file.close()
+    return countries
+
+file = readFile()
+
+def writeFile(countries):
+    file = open(fileName, "w")
+    json.dump(countries, file)
+    file.close()
+
 @app.route("/")
 def index():
     return "<h1><i>Hello everyone! :D</i></h1><br><br><br><h3><b>Feel free to do whatever you want on this page.</b></h3>"
 
 @app.get("/countries")
 def getCountries():
-    return jsonify(countries)
+    return jsonify(file)
 
 @app.get("/countries/<int:id>")
 def getCountry(id):
-    for country in countries:
+    for country in file:
         if country["id"] == id:
             return country, 200
     return {"error": "The following country was not found!"}, 404
 
 def findNextId():
-    return max(country["id"] for country in countries) + 1
+    return max(country["id"] for country in readFile()) + 1
 
 @app.post("/countries")
 def addCountry():
     if request.is_json:
         country = request.get_json()
         country["id"] = findNextId()
-        countries.append(country)
+        file.append(country)
+        writeFile(file)
         return country, 201
     return {"error": "Request must be a JSON file!"}, 415
 
@@ -33,10 +49,11 @@ def addCountry():
 def modifyCountry(id):
     if request.is_json:
         newCountry = request.get_json()
-        for country in countries:
+        for country in file:
             if country["id"] == id:
                 for attribute in newCountry:
                     country[attribute] = newCountry[attribute]
+                    writeFile(file)
                 return country, 200
     return {"error": "Request must be a JSON file!"}
 
@@ -45,10 +62,11 @@ def modifyCountry(id):
 def modifyCountryAtt(id):
     if request.is_json:
         newCountry = request.get_json()
-        for country in countries:
+        for country in file:
             if country["id"] == id:
                 for attribute in newCountry:
                     country[attribute] = newCountry[attribute]
+                    writeFile(file)
                 return country, 200
     return {"error": "Request must be a JSON file!"}
 
