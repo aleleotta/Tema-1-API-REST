b19512ab8de4ee64b43cde24319891a2036411bc
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-10-24 12:37:42 +0200

    .

--- ClientProduct API/Client/Main.py
+++ ClientProduct API/Client/Main.py
@@ -1,2 +1,35 @@
 from Functions import *
 from Petitions import *
+
+option = 10
+print("Welcome!")
+while option != 0:
+    list()
+    try:
+        option = int(input("Introduce an option from above: "))
+    except:
+        print("The option must be an integer!")
+    if option == 1:
+        pass
+    elif option == 2:
+        pass
+    elif option == 3:
+        pass
+    elif option == 4:
+        pass
+    elif option == 5:
+        pass
+    elif option == 0:
+        print("Have a great day!")
+    else:
+        print("Please introduce a valid option!")
+
+
+def list():
+    print("CRUD List:")
+    print("    1) Add")
+    print("    2) Show list")
+    print("    3) Search")
+    print("    4) Update ")
+    print("    5) Delete")
+    print("    0) Exit")
