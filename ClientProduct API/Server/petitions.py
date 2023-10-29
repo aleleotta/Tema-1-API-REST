3536474720104e41b4c6acbb467c06dcaae4939c
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-10-29 02:57:42 +0200

    .

--- ClientProduct API/Server/petitions.py
+++ ClientProduct API/Server/petitions.py
@@ -1,4 +1,5 @@
 from flask import *
+import json
 
 @app.get("")
 def get():
--- ClientProduct API/Server/run.py
+++ ClientProduct API/Server/run.py
@@ -1,15 +1,32 @@
 from flask import *
+import json
 
 app = Flask(__name__)
 
 fileName1 = "JSON//Client.json"
 fileName2 = "JSON//Product.json"
 
-def readFile(fileName):
-    pass
+def readFile1(fileName1):
+    file = open(fileName1, "r")
+    clients = json.load(file)
+    file.close()
+    return clients
 
-def writeFile(fileName):
-    pass
+def readFile2(fileName2):
+    file = open(fileName2, "r")
+    products = json.load(file)
+    file.close()
+    return products
+
+def writeFile1(clients):
+    file = open(fileName1, "w")
+    json.dump(clients, file)
+    file.close()
+
+def writeFile2(products):
+    file = open(fileName2, "w")
+    json.dump(products, file)
+    file.close()
 
 @app.route("/")
 def index():
