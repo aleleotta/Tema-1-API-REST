d6cf217447ad82aac73ae7585094913d2113a26e
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-02-16 12:01:15 +0100

    .

--- d.py
+++
@@ -1,6 +0,0 @@
-import random
-
-password = "" # Password declaration.
-for i in range(30): # Character generation goes up to 30.
-    password = password + chr(random.randint(33, 255)) # chr(index) is used to get the character from the ASCII table with the index. The inverted function is ord(char).
-print(password)
